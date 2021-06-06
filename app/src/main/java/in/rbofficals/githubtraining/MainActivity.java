83fcdf58e89744611f74cb7f168b10600c75dc31
Inzamul Hoque <inzzamol@live.com>
2021-06-06 14:37:45 +0530

    MainActivities

--- app/src/main/java/in/rbofficals/githubtraining/MainActivity.java
+++ app/src/main/java/in/rbofficals/githubtraining/MainActivity.java
@@ -3,6 +3,7 @@ package in.rbofficals.githubtraining;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //THis is log
+        Log.d("TAG", "onCreate: Log Generated");
     }
 }
